ea9f49d08da2cb4694d5c02a21f50bad930998a0
Aman Bansal <amanbansal1997@gmail.com>
2023-01-11 23:43:58 -0800

    Added dummy project pictures for display in the work page. Added instructions in the README on how-to install and run the project and a demo gif.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # astral-blade
+
+🚧 This site is under construction
+
+## Setup
+
+Download [Node.js](https://nodejs.org/en/download/).
+After that, run the followed commands:
+
+```bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+```
+
+![Site Demo](./site-demo.gif)
--- src/experience/projects.js
+++ src/experience/projects.js
@@ -3,31 +3,31 @@ export default [
     {
         name: "project-1",
         description: "project-1-desc",
-        image_src: "assets/allegra.jpg",
+        image_src: "assets/leaves.jpeg",
         ref: "out-link-1",
     },
     {
         name: "project-2",
         description: "project-2-desc",
-        image_src: "assets/barb.jpg",
+        image_src: "assets/lion.jpg",
         ref: "out-link-2",
     },
     {
         name: "project-3",
         description: "project-3-desc",
-        image_src: "assets/cherry.jpg",
+        image_src: "assets/road.jpg",
         ref: "out-link-3",
     },
     {
         name: "project-4",
         description: "project-4-desc",
-        image_src: "assets/maria.jpg",
+        image_src: "assets/color-dragon.jpeg",
         ref: "out-link-4",
     },
     {
         name: "project-5",
         description: "project-5-desc",
-        image_src: "assets/diamond.png",
+        image_src: "assets/astronaut.png",
         ref: "out-link-5",
     },
 ];
